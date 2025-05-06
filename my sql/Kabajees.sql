d7753f4f5d88002af403525e5c3a12b83c392478
sami2602 <sami2407f@aptechsite.net>
2025-05-06 14:41:50 +0500

    .
    .

--- my sql/Kabajees.sql
+++ my sql/Kabajees.sql
@@ -16,4 +16,20 @@ values("Chocolate Biscuit",5,50),
 ("Strawberry Cake",300,5),
 ("Brownies",15,30),("Pastries",15,40);
 
+SELECT * FROM kababjeesbakers.bakeryitems;
+alter table BakeryItems auto_increment = 1001;  -- before inserting data
+truncate table bakeryitems;
+SELECT * FROM BakeryItems WHERE Item_Price <100;
+SELECT Item_Name, Item_Quantity FROM BakeryItems WHERE Item_Price>300;
+SELECT * FROM BakeryItems WHERE Item_Quantity<20;
+UPDATE BakeryItems SET Item_Quantity = 8 WHERE Item_Name = 'Ice Cake';
+UPDATE BakeryItems SET Item_Price = 12 WHERE Item_Name = 'Brownies';
+UPDATE BakeryItems SET Item_Price = 6, Item_Quantity = 20 WHERE Item_Name = 'Chocolate biscuit';
+UPDATE BakeryItems SET Item_Name = 'Chocolate Pastries' WHERE Item_Number = 1007;
+SELECT * FROM BakeryItems WHERE Item_Name LIKE '%e';
+SELECT * FROM BakeryItems WHERE Item_Name LIKE '%n%';
+SELECT * FROM BakeryItems WHERE Item_Name LIKE 'b%';
+DELETE FROM BakeryItems WHERE Item_Quantity < 10;
+
+
 
