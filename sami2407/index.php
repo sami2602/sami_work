b9827dfad64d6a3244b284eefe3d26f3ef45f02b
Kashif0955 <sami2407f@aptechsite.net>
2025-05-29 20:37:56 +0500

    .
    .

---
+++ sami2407/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+echo "hello world (echo) ! " ."<br>";
+print "hello world (print)! "."<br>";
+
+
+$name = "Sami";
+$age = 27;
+
+echo "My Name Is $name and I am $age years old."."<br>";
+
+$num1= 2;
+$num2= 5;
+
+
+$sum= $num1+$num2;
+    echo "$sum" ."<br>";//method 1//
+    echo "the sum of $num1 and $num2 is " .($num1 + $num2) ."<br>";   //method 2//
+
+$multi=$num1*$num2;
+    echo "$multi"."<br>";
+    echo "the multiplication of $num1 and $num2 is " .($num1 * $num2) ."<br>";
+
+?>
