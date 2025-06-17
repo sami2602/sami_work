285724ef470bfea752a76042bbd8fe96f00c44a1
sami <179842079+sami2602@users.noreply.github.com>
2025-06-17 20:34:47 +0500

    .
    .

---
+++ sami2407/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="welcome.php" method="POST">
+    <input type="text" name="u_name" id="" placeholder="Enter Your Name">
+     <input type="text" name="u_email" id="" placeholder="Enter Your Email">
+     <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
---
+++ sami2407/welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<p> Hello <?php echo $_POST['u_name']?> ! </p>
+<p> Your Email is <?php echo $_POST['u_email']?> ! </p>
+
+</body>
+</html>
